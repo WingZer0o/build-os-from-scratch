d8fe658d0d32befea08ddd623164771accd19813
Mike Mulchrone <mtmulch0191@outlook.com>
2022-12-03 22:27:40 -0500

    32bit-main

--- 10-32bit-enter/32bit-main.asm
+++ 10-32bit-enter/32bit-main.asm
@@ -1,12 +1,12 @@
-[org 0x7c00]
-mov bp, 0x9000 ; set the track
-mov sp, bp
+[org 0x7c00] ; bootloader offset
+    mov bp, 0x9000 ; set the stack
+    mov sp, bp
 
-mov bx, MSG_REAL_MODE
-call print ; This will be written after the BIOS message
+    mov bx, MSG_REAL_MODE
+    call print ; This will be written after the BIOS messages
 
-call switch_to_pm
-jmp $ ; this will actually never be executed
+    call switch_to_pm
+    jmp $ ; this will actually never be executed
 
 %include "../05-bootsector-functions-strings/boot_sect_print.asm"
 %include "../09-32bit-gdt/32bit-gdt.asm"
@@ -14,10 +14,10 @@ jmp $ ; this will actually never be executed
 %include "32bit-switch.asm"
 
 [bits 32]
-BEGIN_PM ; after the switch we will get here
+BEGIN_PM: ; after the switch we will get here
     mov ebx, MSG_PROT_MODE
     call print_string_pm ; Note that this will be written at the top left corner
-    jmp $;
+    jmp $
 
 MSG_REAL_MODE db "Started in 16-bit real mode", 0
 MSG_PROT_MODE db "Loaded 32-bit protected mode", 0
