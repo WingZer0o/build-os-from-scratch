f15794597842d44a1d07bad735683a21fa3e5174
Mike Mulchrone <mtmulch0191@outlook.com>
2022-12-04 19:57:08 -0500

    makefile and linking bootsector and kernel

---
+++ 13-kernel-barebones/kernel.c
@@ -0,0 +1,9 @@
+/* This will force us to create entry function instead of jumping to kernel.c 0x00 */
+void dummy_test_entrypoint() {
+
+}
+
+void main() {
+    char* video_memory = (char*) 0xb8000;
+    *video_memory = 'X';
+}
